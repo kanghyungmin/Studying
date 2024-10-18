5bb0681f34416e5fb14178d434409b4f7a9f0565
hyungmin <hmkkang0922@daum.net>
2024-10-18 09:58:00 +0900

    add chpater7-graph source

--- chapter7-Graph/main.go
+++ chapter7-Graph/main.go
@@ -116,37 +116,108 @@ func dfsVisit(g *Graph, u *Node, visit map[int]bool) {
 	}
 }
 
+type WeightedEdge struct {
+	u, v   Node
+	weight int
+}
+
+type WeightedGraph struct {
+	nodes map[Node]struct{}
+	edges map[WeightedEdge]struct{}
+}
+
+func NewWeightedGraph() *WeightedGraph {
+	return &WeightedGraph{
+		nodes: make(map[Node]struct{}),
+		edges: make(map[WeightedEdge]struct{}),
+	}
+}
+
+func (g *WeightedGraph) AddNode(n Node) {
+	g.nodes[n] = struct{}{}
+}
+
+func (g *WeightedGraph) AddEdge(u, v Node, weight int) {
+	g.edges[WeightedEdge{u, v, weight}] = struct{}{}
+}
+
+func (wg *WeightedGraph) RemoveNode(n Node) {
+	delete(wg.nodes, n)
+
+	for e := range wg.edges {
+		if e.u == n || e.v == n {
+			delete(wg.edges, e)
+		}
+	}
+}
+
+type MST struct {
+	nodes map[Node]struct{}
+	edges map[WeightedEdge]struct{}
+}
+
+func (m MST) Print() {
+	fmt.Println("Nodes: ")
+	for n := range m.nodes {
+		fmt.Printf("%v ", n.value)
+	}
+	fmt.Println()
+	fmt.Println("Edges: ")
+	for e := range m.edges {
+		fmt.Printf("%v ", e)
+	}
+}
+
+func Prim(wg *WeightedGraph, start *Node) *MST {
+	treeNodes := make(map[Node]struct{})
+	treeEdges := make(map[WeightedEdge]struct{})
+
+	treeNodes[*start] = struct{}{}
+
+	for len(treeNodes) != len(wg.nodes) {
+		node, minEdge := minEdge(wg, treeNodes)
+		treeNodes[node] = struct{}{}
+		treeEdges[minEdge] = struct{}{}
+	}
+	return &MST{treeNodes, treeEdges}
+}
+func minEdge(wg *WeightedGraph, nodes map[Node]struct{}) (n Node, we WeightedEdge) {
+	min := 10000
+	for e := range wg.edges {
+		_, ok1 := nodes[e.u]
+		_, ok2 := nodes[e.v]
+		if ok1 && !ok2 && e.weight < min {
+			min = e.weight
+			n = e.v
+			we = e
+		}
+	}
+	return
+}
+
 func main() {
 	node27 := NewNode(27)
 	node18 := NewNode(18)
 	node21 := NewNode(21)
 	node9 := NewNode(9)
 	node5 := NewNode(5)
-	node25 := NewNode(25)
+	// node25 := NewNode(25)
 
-	g := New()
-	g.AddNode(node27)
-	g.AddNode(node18)
-	g.AddNode(node21)
-	g.AddNode(node9)
-	g.AddNode(node5)
-	g.AddNode(node25)
+	wg := NewWeightedGraph()
+	wg.AddNode(node21)
+	wg.AddNode(node27)
+	wg.AddNode(node18)
+	wg.AddNode(node5)
+	wg.AddNode(node9)
+	wg.AddEdge(node21, node27, 5)
+	wg.AddEdge(node21, node18, 10)
+	wg.AddEdge(node27, node18, 25)
 
-	g.AddEdge(node27, node18)
-	g.AddEdge(node27, node21)
+	wg.AddEdge(node27, node5, 15)
+	wg.AddEdge(node18, node9, 20)
+	wg.AddEdge(node5, node9, 30)
 
-	g.AddEdge(node27, node9)
-	g.AddEdge(node18, node5)
-
-	g.AddEdge(node21, node5)
-	g.AddEdge(node21, node25)
-
-	g.AddEdge(node9, node25)
-	g.AddEdge(node5, node25)
-
-	fmt.Println("BFS")
-	BFS(g, &node27)
-	fmt.Println("DFS")
-	DFS(g, &node27)
+	mstPrim := Prim(wg, &node21)
+	mstPrim.Print()
 
 }
